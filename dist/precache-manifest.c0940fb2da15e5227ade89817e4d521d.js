1e701be936266bcce9a350e1d4ee9f0e6737eba0
Nini0604 <sooy4503@gmail.com>
2019-12-15 16:49:41 +0900

    dist폴더추가

---
+++ dist/precache-manifest.c0940fb2da15e5227ade89817e4d521d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03807b8573c65d7887d",
+    "url": "/css/app.748686b6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "449847764530502e41d143955593535b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d03807b8573c65d7887d",
+    "url": "/js/app.dbcca578.js"
+  },
+  {
+    "revision": "b622148619b5466a236e",
+    "url": "/js/chunk-vendors.b18df113.js"
+  },
+  {
+    "revision": "af4a8287fe9ccfd409c96c143f922eca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
